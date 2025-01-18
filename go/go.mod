ccada777391212c2be313147869e51ffd760cdac
Juhun <riann3207@gmail.com>
2025-01-17 23:59:16 -0500

    refactor: remove deprecated OAuth configuration and improve config loading

--- go/go.mod
+++ go/go.mod
@@ -3,9 +3,12 @@ module jtracker-backend
 go 1.23.1
 
 require (
+	github.com/gorilla/mux v1.8.1
+	golang.org/x/oauth2 v0.25.0
+)
+
+require (
 	cloud.google.com/go/compute/metadata v0.6.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/oauth2 v0.25.0 // indirect
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/sys v0.29.0 // indirect
 )
--- go/main.go
+++ go/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"jtracker-backend/config"
 	"jtracker-backend/handlers"
-	// "log"
+	"log"
 	"net/http"
-	// "os"
 
 	"github.com/gorilla/mux"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// googleAccessKey := os.Getenv("GOOGLE_CLIENT_ID")
+	// googleSecretKey := os.Getenv("GOOGLE_CLIENT_SECRET")
+
+	config.LoadConfig()
 
 	// Initialize the router
 	r := mux.NewRouter()
@@ -18,5 +28,9 @@ func main() {
 	r.HandleFunc("/login", handlers.LoginHandler)
 	r.HandleFunc("/auth/callback", handlers.CallbackHandler)
 
-	http.ListenAndServe(":8080", r)
+	port := ":8080"
+	fmt.Println("Server started at http://localhost" + port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
