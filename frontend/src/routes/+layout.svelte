ab546809fd4b6bd337a1239217f71ba2485d56af
Sean Kim <sean.kim0327@gmail.com>
2025-01-17 23:43:09 -0500

    home page and signin

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,30 +6,35 @@
 
     import { Separator } from "$lib/components/ui/separator";
     import { Button } from "$lib/components/ui/button";
+
+    import { Moon } from 'lucide-svelte';
+    import { SunMedium } from 'lucide-svelte';
 </script>
 
 <ModeWatcher />
-<header class="fixed top-0 left-0 right-0 bg-background z-50">
-    <div class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div class="flex-none">
-            <a href="/" class="text-2xl font-bold hover:underline">
-                jtracker
-            </a>
+<div class="min-h-screen grid grid-rows-[auto_1fr]">
+    <header class="bg-background z-50">
+        <div class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+            <div class="flex-none">
+                <a href="/" class="text-2xl font-bold hover:underline">
+                    jtracker
+                </a>
+            </div>
+            <div class="flex items-center gap-4 mx-8">
+                <Button variant="ghost">Profile</Button>
+                <Separator orientation="vertical" class="h-6" />
+                <Button variant="ghost">Postings</Button>
+            </div>
+            <div class="flex-none">
+                <Button on:click={toggleMode} variant="outline" class="w-[4rem]">
+                    <SunMedium class="dark:hidden"/>
+                    <Moon class="hidden dark:block"/>
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </div>
         </div>
-        <div class="flex items-center gap-4 mx-8">
-            <Button variant="ghost">Profile</Button>
-            <Separator orientation="vertical" class="h-6" />
-            <Button variant="ghost">Postings</Button>
-        </div>
-        <div class="flex-none">
-            <Button on:click={toggleMode} variant="outline" class="w-[4rem]">
-                <p class="dark:hidden">Light</p>
-                <p class="hidden dark:block">Dark</p>
-                <span class="sr-only">Toggle theme</span>
-            </Button>
-        </div>
-    </div>
-</header>
-<main class="bg-background h-screen flex justify-center items-center">
-    {@render children()}
-</main>
+    </header>
+    <main class="bg-background overflow-auto">
+        {@render children()}
+    </main>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,17 +9,17 @@
     }
 </script>
 
-<div class="flex flex-col justify-center gap-12 items-center h-screen">
-    <h1 class="text-4xl font-bold text-center w-3/4">
-        The #1 platform for students in tech to track 
+<div class="flex flex-col justify-center gap-4 items-center h-full">
+    <h1 class="text-4xl font-bold text-center w-1/2">
+        Streamline your <span class="text-violet-500">tech internship</span> hunt.
     </h1>
-    <p class="text-xl text-center">
-        Track your applications, see new postings in real time, and land your dream internship.
+    <p class="text-xl text-center w-1/2">
+        Track applications, discover new opportunities, and jumpstart your career.
     </p>
-    <Card.Root class="flex flex-row">
+    <Card.Root class="flex flex-row mt-16">
         <Card.Header>
-        <Card.Title>Create an account</Card.Title>
-            <Card.Description>It's completely free!</Card.Description>
+        <Card.Title>Created by students, for students.</Card.Title>
+            <Card.Description>Get started; it's completely free!</Card.Description>
         </Card.Header>
         <Card.Content>
             <p>
