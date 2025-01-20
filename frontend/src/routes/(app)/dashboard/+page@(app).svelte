6bc59a4ebef5fbed5ca5f116215e18130b0ce6f5
Juhun <riann3207@gmail.com>
2025-01-20 11:19:38 -0500

    feat: add job dashboard component with mock data and update layout

--- frontend/src/routes/(app)/dashboard/+page@(app).svelte
+++ frontend/src/routes/(app)/dashboard/+page@(app).svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
     export let data;
+
+    // job component
+    import Job from "./job.svelte";
+
+    // mock data json file
+    import mockData from "./mockdata.json";
+    const jobs = mockData.jobs.map((job) => ({
+        ...job,
+        appliedDate: new Date(job.appliedDate),
+    }));
 </script>
 
 <div class="flex flex-col justify-center gap-4 items-center h-full">
     <h1>Dashboard</h1>
     {#if data.email}
         <pre>{JSON.stringify(data.email, null, 2)}</pre>
+        <div class="w-full max-w-3xl space-y-4 text-black">
+            {#each jobs as job (job.id)}
+                <Job
+                    id={job.id}
+                    company={job.company}
+                    role={job.role}
+                    appliedDate={job.appliedDate}
+                    location={job.location}
+                    status={job.status}
+                />
+            {/each}
+        </div>
     {:else}
         <p>Not logged in</p>
     {/if}
---
+++ frontend/src/routes/(app)/dashboard/job.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let id: string;
+    export let company: string;
+    export let role: string;
+    export let appliedDate: Date;
+    export let location: string;
+    export let status = "Applied";
+</script>
+
+<div class="flex flex-col justify-center gap-4 items-center">
+    <div class="flex flex-col justify-center gap-4 items-center p-4 rounded-lg shadow-">
+        <p>Id: {id}</p>
+        <p>Company: {company}</p>
+        <p>Role: {role}</p>
+        <p>Applied Date: {appliedDate.toDateString()}</p>
+        <p>Location: {location}</p>
+        <p>Status: {status}</p>
+    </div>
+</div>
