a9825434499bdc6a4c68212f47b643c1066bdd90
Juhun <riann3207@gmail.com>
2025-03-03 23:06:27 -0500

    feat(frontend): home page layout added dashed border

--- frontend/src/routes/(home)/+page.svelte
+++ frontend/src/routes/(home)/+page.svelte
@@ -9,27 +9,51 @@
     }
 </script>
 
-<div class="container flex flex-col justify-center gap-4 items-center h-full border-x border-dashed">
-    <h1 class="text-2xl sm:text-4xl font-bold text-center w-3/4 sm:w-1/2">
-        Streamline your <span class="text-violet-500">tech internship</span> hunt.
-    </h1>
-    <p class="text-base sm:text-xl text-center w-3/4 sm:w-1/2">
-        Track applications, discover new opportunities, and jumpstart your
-        career.
-    </p>
-    <Card.Root class="flex flex-col items-center sm:items-start sm:flex-row mt-16">
-        <Card.Header>
-            <Card.Title>Created by students, for students.</Card.Title>
-            <Card.Description
-                >Get started; it's completely free!</Card.Description
+<div class="container-wrapper grid grid-rows-[1fr_1fr] h-full">
+    <div class="h-full w-full">
+        <div
+            class="container h-full w-full flex flex-col justify-end pb-16 items-center border-x border-dashed"
+        >
+            <h1
+                class="text-2xl sm:text-4xl font-bold text-center w-3/4 sm:w-1/2"
             >
-        </Card.Header>
-        <Card.Content>
-            <p>
-                <Button on:click={handleGoogleSignIn}>
-                    <Fa icon={faGoogle} /> &nbsp; Sign in with Google
-                </Button>
+                Streamline your <span class="text-violet-500"
+                    >tech internship</span
+                >
+                hunt.
+            </h1>
+            <p class="text-base sm:text-xl text-center w-3/4 sm:w-1/2">
+                Track applications, discover new opportunities, and jumpstart
+                your career.
             </p>
-        </Card.Content>
-    </Card.Root>
+        </div>
+        <div class="border-y border-dashed">
+            <div
+                class="container flex justify-center py-8"
+            >
+                <div class="w-fit">
+                    <Card.Root
+                        class="flex flex-col items-center sm:items-start sm:flex-row"
+                    >
+                        <Card.Header>
+                            <Card.Title
+                                >Created by students, for students.</Card.Title
+                            >
+                            <Card.Description
+                                >Get started; it's completely free!</Card.Description
+                            >
+                        </Card.Header>
+                        <Card.Content>
+                            <p>
+                                <Button on:click={handleGoogleSignIn}>
+                                    <Fa icon={faGoogle} /> &nbsp; Sign in with Google
+                                </Button>
+                            </p>
+                        </Card.Content>
+                    </Card.Root>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container h-full border-x border-dashed"></div>
 </div>
